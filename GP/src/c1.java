51f08757052c3db4f7d7149b64e1c3daade89181
divansh <divanshar@gmail.com>
2016-10-24 16:04:29 +0530

    First commit

---
+++ GP/src/c1.java
@@ -0,0 +1,36 @@
+import java.awt.List;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class c1 {
+	
+	void say(ArrayList<? extends Number> list) { 
+    	System.out.println("parent");
+}
+	
+	public static void main(String[] args) {
+		
+		ArrayList<? extends Number> list1 = null;
+		ArrayList<Integer> list2 = null;
+		list1 = list2;
+		
+		
+	}
+	}
+
---
+++ GP/src/poo.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class poo extends c1 {
+
+	void say(ArrayList<? extends Number> list) {
+   		System.out.println("child");
+}
+}
